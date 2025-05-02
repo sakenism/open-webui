cae654a45c7323738242f8d2587738cdbdb6e15b
Saken Mukanov <saken.mukanov@nu.edu.kz>
2025-05-02 12:04:57 +0500

    upd: switch off sync.sh

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
-while true; do
-  sleep 1
-  rm -rf /shared/uploads/* && cp -r /app/backend/data/uploads /shared/
-  rm -rf /shared/webui.db && cp /app/backend/data/webui.db /shared/webui.db
-  sleep 1
-done
+# while true; do
+#   sleep 1
+#   rm -rf /shared/uploads/* && cp -r /app/backend/data/uploads /shared/
+#   rm -rf /shared/webui.db && cp /app/backend/data/webui.db /shared/webui.db
+#   sleep 1
+# done
